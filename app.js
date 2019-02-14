c3f2cf786a47c5eda0a6c700df5c754449c94f6b
Edward McCarthy <efmccarthy26@gmail.com>
2019-02-14 11:56:43 -0700

    Declared Event type and now returning Event objects instead

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
-
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
