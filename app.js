54bbcc6b8b5797b5e8204985bc40096401bdd8d8
Edward McCarthy <efmccarthy26@gmail.com>
2019-02-14 11:11:56 -0700

    Can query and get the array of pre-loaded events from graphiQL

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hey")
-})
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ["Cooking in Pairs", 'All Night Flogging', "Book Writign Seminar"]
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(3000);
