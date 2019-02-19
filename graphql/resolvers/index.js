cee544083458779e06872167de981e0059055714
Edward McCarthy <efmccarthy26@gmail.com>
2019-02-18 20:57:53 -0700

    Single event, with the event being shown for booking creation return

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -21,6 +21,19 @@ const events = async eventIds => {
   }
 }
 
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator)
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
 const user = async userId => {
   try {
     const user = await User.findById(userId)
@@ -58,6 +71,8 @@ module.exports = {
         return {
           ...booking._doc,
           _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
           createdAt: new Date(booking._doc.createdAt).toISOString(),
           createdAt: new Date(booking._doc.updatedAt).toISOString()
         }
