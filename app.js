245ab7199dc5f95e11a648894f3c1fab9e4ad3b7
mcshakes <efmccarthy26@gmail.com>
2019-04-08 15:46:00 -0600

    Will never return an array of null objects. Returning the name of the Session back

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphQLHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use("/graphql", graphQLHTTP({
+	schema: buildSchema(`
+		type RootQuery {
+			sessions: [String!]! 
+		}
+
+		type RootMutation {
+			createSession(name: String): String
+		}
+
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {}
+}));
 
 app.listen(3000);
