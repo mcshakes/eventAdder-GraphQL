6914e6d42b935e8bc9694c919d423846c0065b28
mcshakes <efmccarthy26@gmail.com>
2019-04-08 15:57:10 -0600

    appointment sounds clinical, changed to name. added graphiql interface

--- app.js
+++ app.js
@@ -7,22 +7,35 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/graphql", graphQLHTTP({
-	schema: buildSchema(`
-		type RootQuery {
-			sessions: [String!]! 
-		}
+app.use(
+	"/graphql", 
+	graphQLHTTP({
+		schema: buildSchema(`
+			type RootQuery {
+				events: [String!]! 
+			}
 
-		type RootMutation {
-			createSession(name: String): String
-		}
+			type RootMutation {
+				createEvent(name: String): String
+			}
 
-		schema {
-			query: RootQuery
-			mutation: RootMutation
-		}
-	`),
-	rootValue: {}
-}));
+			schema {
+				query: RootQuery
+				mutation: RootMutation
+			}
+		`),
+		rootValue: {
+			events: () => {
+				return ["Romantic Cooking", "Group Therapy", "fight club..."]
+			},
+			createEvent: (args) => {
+				const eventName = args.name;
+
+				return eventName;
+			}
+		},
+		graphiql: true
+	})
+);
 
 app.listen(3000);
