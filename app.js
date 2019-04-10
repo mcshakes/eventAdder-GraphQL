82529857d2d64dace662d6fcfa5f737e808d627f
mcshakes <efmccarthy26@gmail.com>
2019-04-10 08:44:00 -0600

    Events model and the can Create and Get all

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const Event = require("./models/event");
 
 const app = express();
 
-const events = [
-
-];
 
 app.use(bodyParser.json());
 
@@ -48,16 +45,19 @@ app.use(
 		`),
 		rootValue: {
 			events: () => {
-				return events;
+				return Event
+					.find()
+					.then(events => {
+						return events.map(event => {
+							return { ...event._doc };
+						})
+					})
+					.catch(err => {
+						throw err;
+					})
 			},
 			createEvent: (args) => {
-				// const event = {
-				// 	_id: Math.random().toString(),
-					// title: args.eventInput.title,
-					// description: args.eventInput.description,
-					// price: +args.eventInput.price,
-					// date: args.eventInput.date
-				// }
+
 				const event = new Event({
 					title: args.eventInput.title,
 					description: args.eventInput.description,
@@ -65,8 +65,18 @@ app.use(
 					date: new Date(args.eventInput.date)
 				});
 
-				events.push(event);
-				return event;
+				
+				return event
+					.save()
+					.then(result => {
+						console.log(result);
+						return {...result._doc};
+					})
+					.catch(err => {
+						console.log(err);
+						throw err;
+					});
+
 			}
 		},
 		graphiql: true
