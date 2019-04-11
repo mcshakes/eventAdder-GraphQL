42f8c71a4f057c5a6beecd0639b200b99932f0b5
mcshakes <efmccarthy26@gmail.com>
2019-04-11 16:37:46 -0600

    Attaching relations between createdEvents and the User; hardcoding User ID for now, for my one created guy.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const graphQLHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -23,11 +25,22 @@ app.use(
 				date: String!
 			}
 
+			type User {
+				_id: ID!
+				email: String!
+				password: String 
+			}
+
 			input EventInput {
 				title: String!
 				description: String!
 				price: Float!
 				date: String!
+			}
+
+			input UserInput {
+				email: String!
+				password: String!
 			}
 
 			type RootQuery {
@@ -36,6 +49,7 @@ app.use(
 
 			type RootMutation {
 				createEvent(eventInput: EventInput): Event
+				createUser(userInput: UserInput): User
 			}
 
 			schema {
@@ -62,20 +76,61 @@ app.use(
 					title: args.eventInput.title,
 					description: args.eventInput.description,
 					price: +args.eventInput.price,
-					date: new Date(args.eventInput.date)
+					date: new Date(args.eventInput.date),
+					creator: "5cafbd23fe0264ff3064f0ca"
 				});
 
 				
 				return event
 					.save()
 					.then(result => {
+						return User.findById("5cafbd23fe0264ff3064f0ca")
 						console.log(result);
 						return {...result._doc};
+					})
+					.then(user => {
+						if (user) {
+							throw new Error("User exists already.")
+						}
+
+						user.createEvents.push(event);
 					})
 					.catch(err => {
 						console.log(err);
 						throw err;
 					});
+
+			},
+
+			createUser: args => {
+				
+				return User.findOne({ email: args.userInput.email })
+					.then(user => {
+						// user is undefined or an object, hence exists
+
+						if (user) {
+							throw new Error("User exists already.")
+						}
+
+						return bcrypt.hash(args.userInput.password, 12)
+					})
+					.then(hashedPassword => {
+						const user = new User({
+							email: args.userInput.email,
+							password: hashedPassword
+						})
+
+						return user.save();
+					})
+					.then(result => {
+						// override to return null for the password. 
+
+						return { ...result._doc, password: null }
+					})
+					.catch(err => {
+						throw err;
+					})
+
 
 			}
 		},
