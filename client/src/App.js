037006f1380112a0386b3979d975fb554ab93551
mcshakes <efmccarthy26@gmail.com>
2019-07-07 17:07:25 -0600

    More routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import AuthPage from "./components/Auth";
+import BookingsPage from "./components/Bookings";
+import EventsPage from "./components/Events";
 
 import './App.css';
 
@@ -11,8 +13,8 @@ function App() {
       <Switch>
         <Redirect from="/" to="/auth" exact/>
         <Route path="/auth" component={ AuthPage } />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/events" component={ EventsPage } />
+        <Route path="/bookings" component={ BookingsPage } />
       </Switch>
 
     </BrowserRouter>
---
+++ client/src/components/Events.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class EventsPage extends React.Component {
+	render() {
+		return (
+			<h1>The Events Page</h1>
+		);
+	}
+}
+
+export default EventsPage;
