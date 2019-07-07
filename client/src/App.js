434e5c257c8e065e92e350df56f0f2ae88d008eb
mcshakes <efmccarthy26@gmail.com>
2019-07-07 17:04:54 -0600

    Setting up auto redirect to auth; Events adn Bookings Components for now before moving on

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./components/Auth";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact/>
+        <Route path="/auth" component={ AuthPage } />
+        <Route path="/events" component={null} />
+        <Route path="/bookings" component={null} />
+      </Switch>
+
+    </BrowserRouter>
+
   );
 }
 
