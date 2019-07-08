5c1db959e66a7e8306bcf97960fe37947c893b3e
mcshakes <efmccarthy26@gmail.com>
2019-07-08 16:55:38 -0600

    Changed from function to component

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,39 @@ import BookingsPage from "./components/Bookings";
 import EventsPage from "./components/Events";
 
 import MainNavigation from "./components/navigation/MainNavigation";
+import AuthContext from "./context/auth-context";
 
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
+class App extends React.Component {
+  
+  login = (token, userId, tokenExpiration) => {
 
-      <main className="main-content">
-        <Switch>
-          <Redirect from="/" to="/auth" exact/>
-          <Route path="/auth" component={ AuthPage } />
-          <Route path="/events" component={ EventsPage } />
-          <Route path="/bookings" component={ BookingsPage } />
-        </Switch>
-        
-      </main>
+  }
 
-    </BrowserRouter>
 
-  );
+  render() {
+    return (
+      <BrowserRouter>
+
+        <AuthContext.Provider value={{token: null, userId: null}}>
+          <MainNavigation />
+
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact/>
+              <Route path="/auth" component={ AuthPage } />
+              <Route path="/events" component={ EventsPage } />
+              <Route path="/bookings" component={ BookingsPage } />
+            </Switch>
+            
+          </main>
+        </AuthContext.Provider>
+
+      </BrowserRouter>
+
+    )
+  };
 }
 
 export default App;
