56ceccbff8a7743dc66264529e9e412e0c642995
mcshakes <efmccarthy26@gmail.com>
2019-07-17 13:19:27 -0600

    Some custom modal effects

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,9 +1,38 @@
 import React from "react";
 
+import Modal from "../components/modals/Modal";
+import Backdrop from "./backdrop/Backdrop";
+import "./Events.css";
+
+
 class EventsPage extends React.Component {
+	state = {
+		creatingStatus: false
+	}
+
+	createEventHandler = () => {
+		this.setState({ creatingStatus: true });
+	}
+
 	render() {
+
+
+
 		return (
-			<h1>The Events Page</h1>
+			<React.Fragment>
+				{this.state.creatingStatus && <Backdrop />}
+				{this.state.creatingStatus && <Modal title="Add Event" canCancel canConfirm>
+					<p>Modal Content</p>
+				</Modal>}
+
+				<div className="events-control">
+					<p>share your events</p>
+
+					<button className="btn" onClick={this.createEventHandler}>
+						Create Event
+					</button>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
---
+++ client/src/components/modals/Modal.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Modal.css";
+
+const modal = props => (
+	<div className="modal">
+		<header className="modal__header">
+			<h1>{props.title}</h1>
+		</header>
+		<section className="modal__content">
+			{props.children}
+		</section>
+
+		<section className="modal__actions">
+			{props.canCancel && <button>Cancel</button>}
+
+			{props.canConfirm && <button>Confirm</button>}
+		</section>
+	</div>
+);
+
+export default modal;
