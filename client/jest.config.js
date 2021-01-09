18cc3e91c2497a3c4e94be250cf263143df48d49
mcshakes <efmccarthy26@gmail.com>
2021-01-09 13:56:10 -0700

    Apparently doesn't like the css files, so that line in jest config is useful

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    "transform": {
-        "^.+\\.jsx?$": "babel-jest"
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    // transform: {
+    //     '^.+\\.js?$': 'babel-jest',
+    // },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "identity-obj-proxy"
     }
 }
