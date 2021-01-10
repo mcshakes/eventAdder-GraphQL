2734234b2257af8556d91360b2690cfd3ee9f3f6
mcshakes <efmccarthy26@gmail.com>
2021-01-10 12:31:29 -0700

    Another test, another package

--- client/__tests__/Auth.spec.js
+++ client/__tests__/Auth.spec.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render, fireEvent, waitFor, screen, cleanup } from '@testing-library/react';
 import { toHaveAttribute } from '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import Auth from '../src/components/Auth';
 
 afterEach(cleanup)
 
 describe('Auth Page', () => {
 
-    // beforeEach(() => {
-    //     const { getByTestId } = render(<Auth />);
-    // });
-
-    test('renders Auth with login by default', () => {
+    test('renders Auth Component with login by default', () => {
         const { getByTestId } = render(<Auth />);
     
         expect(getByTestId('auth-selection')).not.toHaveAttribute('disabled')
@@ -22,5 +19,23 @@ describe('Auth Page', () => {
         const { getByTestId } = render(<Auth />);
     
         expect(getByTestId('auth-submit')).not.toHaveAttribute('disabled')
+    });
+
+    describe('Can change state to Sign In', () => {
+
+        test('it allows user to Sign Up instead of Login', () => {
+            render(<Auth />);
+            
+            userEvent.click(screen.getByText("Switch to Sign Up"))
+            expect(screen.getByTestId('auth-selection')).toHaveTextContent('Switch to Log In')
+        });
+
+        // test('it allows user ti sign up instead of login', () => {
+        //     expect(true).toBe(true);
+        // });
+        
+        // test('false is falsy', () => {
+        //     expect(false).toBe(false);
+        // });
     });
 });
